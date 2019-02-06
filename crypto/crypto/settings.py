e5c963ca6f23b4c975306231b37329380875e862
Alain BERRIER <alain.berrier@outlook.com>
2019-02-06 15:46:21 +0100

    static

--- crypto/crypto/settings.py
+++ crypto/crypto/settings.py
@@ -145,7 +145,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'crypto/static'), os.path.join(BASE_DIR, 'static'), os.path.join('/', 'static'))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'crypto/static'), os.path.join(BASE_DIR, 'static'), os.path.join('/', 'static'), '/')
 # DRF configuration
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (

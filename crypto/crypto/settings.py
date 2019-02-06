ec6903fae60a7619e1d7216e6b6bca1c71b8d9fc
Alain BERRIER <alain.berrier@outlook.com>
2019-02-06 15:34:09 +0100

    static

--- crypto/crypto/settings.py
+++ crypto/crypto/settings.py
@@ -142,7 +142,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/static'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # DRF configuration
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
